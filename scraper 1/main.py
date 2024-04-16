dcd76245407518ca32d1f9e1a836523ff7285faa
kingtroga <trogaclassicman@gmail.com>
2024-04-16 12:06:15 +0100

    initial commit

---
+++ scraper 1/main.py
@@ -0,0 +1,328 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup as bs
+from urllib.parse import urlparse, parse_qs
+import urllib
+from PIL import Image
+from io import BytesIO
+import os
+from datetime import datetime
+import time
+import warnings
+from bs4.builder import XMLParsedAsHTMLWarning
+import warnings
+warnings.filterwarnings('ignore', category=XMLParsedAsHTMLWarning)
+
+
+
+Image.MAX_IMAGE_PIXELS = None
+
+access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI3NWNmNzkyNjIyOGJjM2Y1OTFiMTA3ZmI0MWJiZDQ2YSIsImp0aSI6IjQwMmNmNjk3NTQ5ZWFiYTNmZTM1MDgzZGQzNjM4ODQxNmJmZDZkZWYyNzg4ODBkY2ZhNTk1OTgwMzQxM2U5YjEyMzdjNWY1OTFlM2U4ZTQ4IiwiaWF0IjoxNzA5MjY3NTk0LjA0NDY4NiwibmJmIjoxNzA5MjY3NTk0LjA0NDY5LCJleHAiOjMzMjY2MTc2Mzk0LjA0MzA1Niwic3ViIjoiNzUwOTc4NzkiLCJpc3MiOiJodHRwczovL21ldGEud2lraW1lZGlhLm9yZyIsInJhdGVsaW1pdCI6eyJyZXF1ZXN0c19wZXJfdW5pdCI6NTAwMCwidW5pdCI6IkhPVVIifSwic2NvcGVzIjpbImJhc2ljIl19.BUqJXojS3jo8leOryY-iYt9EdZGMYbPgDwQ8sPnHRqvyuLvWpl64wLZStsnSHQjTIbqd10_lUA4WvaNfjSTTUqjAgLRsCKAtce9-5dbTc7qUc79zPYH77t8wY3o7yJqeFF--ZFC-myTBWX6JgLLHB_sbIWP34IQsOHtQY07e8EDKzyNtRifA6sEwGsrN64NaKkLHdsfwqcaCt47643Ply-05-B8JqagFk0uY1IBBq2VX6Uns0-kdUskMe83aeGEgIMYD5D1KRKHvISbD05MB3AX7mbIsJzl2-gcsyH6gLWYxLwseKK0zooRaVt41nQdJzosTIxSgjTktCOUfIT3sENEUKN0rI1AbisjJr7SBMZOjVNQr1D3lJx_ewRNxhhvKaKmiOK8t1vAS4RJ7mYte-u0vYcK8gSvGdXFXFqtHz9AkQCzfwWFyL5y_zx4561xuiYVQm4Jt1cecaV3qblJPcHwA3K_vzKzZygjJYGr7r5J_YtdlQrlbZU0D6Odssp0jg8GPXSdV5B7-wxAgq-KXP4tumMHGWA5QD_xqxi8JFCbUdGc3gUzsM6nE7g8CQ5LitzoK9nIptrCsKQSF-HOf_KD7x_su35C5kRoXr4wsz5N2IOyeH7_MJSDg9CfY2ayhqg6prbzrWGspHaG1EQrSQVADo6VUzIfOlOgNN55h-54"
+
+
+
+
+def round_to_nearest_thousand(number):
+    # Divide the number by 1000, round it to the nearest integer, and multiply by 1000
+    rounded_number = round(number / 1000) * 1000
+    return rounded_number
+
+def round_to_nearest_hundred(number):
+    # Divide the number by 100, round it to the nearest integer, and multiply by 100
+    rounded_number = round(number / 100) * 100
+    return rounded_number
+
+def get_number_of_articles(url):
+  headers = {
+    "Authorization": f"Bearer {access_token}",
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+  }
+  response = requests.get(url=url, headers=headers)
+  soup = bs(response.text, 'html.parser')
+  results_info_div = soup.find('div', class_='results-info')
+  try:
+    strong_tags = results_info_div.find_all('strong')
+  except:
+    try:
+      noOfArticles = int(input("Sorry Human 🤖, I'm  unable to get the number of articles from the page. Pls tell me the number of articles:> "))
+      return noOfArticles
+    except ValueError:
+      noOfArticles = int(input("Sorry Human 🤖, Pls tell me the number of articles without the Comma, Decimal Point, or any other Punctation:> "))
+      return noOfArticles
+
+
+  noOfArticles = str(strong_tags[1].text).replace(',','')
+  if len(noOfArticles) > 3:
+    noOfArticles = round_to_nearest_thousand(int(noOfArticles))
+  if 3 > len(str(noOfArticles)) > 2:
+    noOfArticles = round_to_nearest_hundred(int(noOfArticles))
+
+  return(str(noOfArticles))
+
+def get_list_of_articles(search_params):
+   headers = {
+    "Authorization": f"Bearer {access_token}",
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+  }
+   articles = []
+   commons_api_url = "https://commons.wikimedia.org/w/api.php"
+   response = requests.get(url=commons_api_url, params=search_params, headers=headers)
+
+   if response.status_code == 200:
+      search_results = response.json()
+      #print(search_results)
+      try:
+        search_results = search_results['query']['search']
+        for result in search_results:
+          articles.append(result['title'])
+      except Exception as e:
+        return "coco"
+      return articles
+   else:
+      return [f'{response.status_code}']
+
+def get_main_list(no_of_articles, search_params): # 10,000 requests with get_list_of_articles
+  main_list = []
+  coco = 0
+  loop_iterator = round(int(no_of_articles)/ 500)# single request limit is 500
+  for i in range(0, loop_iterator+1):
+    search_params['sroffset'] = coco
+    loop_list = get_list_of_articles(search_params=search_params)
+    if loop_list != "coco":
+      for article in loop_list:
+        main_list.append(article)
+      coco = coco + 500
+  main_list = main_list[0: int(no_of_articles)]
+  return main_list
+
+def get_excel_data(image_url):
+    response = requests.get(url=image_url)
+    soup = bs(response.content, 'html.parser')
+    description = ""
+    source = ""
+    date = ""
+    place = ""
+    table_data = soup.find('table', class_='fileinfotpl-type-information')
+
+    # Error handling
+    if table_data is None:
+        table_data = soup.find('table', class_='fileinfotpl-type-artwork')
+
+    try:
+        table_rows = table_data.find_all('tr')
+        for table_row in table_rows:
+            # Description
+            if table_row.find('td', id="fileinfotpl_desc"):
+                try:
+                    description = table_row.find('div', class_="en").text.encode('utf-8').decode('utf-8')
+                except:
+                    description = table_row.find('td', id="fileinfotpl_desc").find_next_sibling().text.encode('utf-8').decode('utf-8')
+            # Date
+            elif table_row.find('td', id="fileinfotpl_date"):
+                date = table_row.find('td', id="fileinfotpl_date").find_next_sibling().text.encode('utf-8').decode('utf-8')
+            # Source
+            elif table_row.find('td', id='fileinfotpl_src'):
+                source = table_row.find('td', id='fileinfotpl_src').find_next_sibling().text.replace('\n', '').encode('utf-8').decode('utf-8')
+
+    except:
+        table_data = soup.find('table', class_="wikitable filehistory")
+        try:
+            table_rows = table_data.find_all('tr')
+        except AttributeError:
+            return None, None, None, None
+        needed_data = table_rows[1].find_all('td')
+
+        description = needed_data[-1].text.encode('utf-8').decode('utf-8')
+        date = needed_data[1].text.encode('utf-8').decode('utf-8')
+
+    # Place
+
+    all_divs = soup.find_all('div', class_="description en")
+    try:
+        i_tag = all_divs[0].find('i')
+    except:
+        i_tag = None
+    try:
+        place = i_tag.find_all('a')[1].text.encode('utf-8').decode('utf-8')
+    except:
+        place = ''
+
+    description = description.strip()
+    source = source.strip()
+    date = date.strip()
+    place = place.strip()
+
+    return description, place, date, source
+
+def get_image_download_url(article_title):
+    headers = {
+    "Authorization": f"Bearer {access_token}",
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+  }
+    # Replace spaces with underscores in the article title
+    formatted_title = article_title.replace(' ', '_')
+
+    # Define the URL
+    url = f"https://commons.wikimedia.org/w/api.php?action=query&format=xml" + \
+        f"&prop=imageinfo&iiprop=url&titles=File:{formatted_title}"
+
+    # Make the request
+    response = requests.get(url=url, headers=headers)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the XML response
+        soup = bs(response.text,'lxml')
+        # Find the URL attribute in the ii tag
+        try:
+          image_url = soup.find('ii')['url']
+        except:
+          image_url = None
+        return image_url
+    else:
+        # Print an error message if the request failed
+        print("Error:", response.status_code)
+        return None
+
+def get_main_image(wiki_link,article, save_dir, IMAGE_NUM):
+  headers = {
+    "Authorization": f"Bearer {access_token}",
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+  }
+  Image.MAX_IMAGE_PIXELS = None
+  cleanArticleName = article.replace(" ", "_").replace("File:", "")
+  image_url = wiki_link + article.replace(" ", "_")  # this is actually an img description url
+  image_name = str(IMAGE_NUM + 1)
+  time.sleep(0.5)
+  response = requests.get(url = image_url, headers = headers)
+  soup = bs(response.text, 'html.parser')
+  img_download_url = get_image_download_url(cleanArticleName)
+  
+
+  if img_download_url:
+    try:
+      image_name = image_name + img_download_url[-4:]
+      urllib.request.urlretrieve(img_download_url, save_dir+"//"+image_name)
+    except Exception as e:
+      try:
+        image_name = image_name + img_download_url[-5:]
+        urllib.request.urlretrieve(img_download_url, save_dir+"//"+image_name)
+      except Exception as e:
+        image_name = ""
+
+  description , place, date, source = get_excel_data(image_url)
+  return image_url, image_name, description , place, date, source
+
+def parse_wikimedia_url(url):
+    parsed_url = urlparse(url)
+
+    query_params = parse_qs(parsed_url.query)
+    if not parsed_url.query: #is it a search link or one link
+      return None, None
+
+    # Extract search query
+    search_query = query_params.get('search', [''])[0]
+
+    # Extract namespaces
+    namespaces = []
+    for key, value in query_params.items():
+        if key.startswith('ns') and value == ['1']:
+            namespaces.append(key)
+
+
+
+    search_params = {
+        "action": "query",
+        "format": "json",
+        "list": "search",
+        "srsearch": search_query,
+        "srlimit": '500',  # Default limit to 500 if not provided
+        "srnamespace": "|".join(namespaces),
+    }
+
+    return search_params, get_number_of_articles(url)
+
+
+# MAIN APP LOGIC
+columns = ['url','title', 'description', 'source', 'place', 'depicted place', 'image_name']
+df = pd.DataFrame(columns=columns)
+
+
+url_scraped = input("Paste your link here:> ")
+print("Please wait...")
+search_params, no_of_articles = parse_wikimedia_url(url_scraped)
+current_datetime = datetime.now()
+save_dir = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")
+
+
+
+if search_params:
+  main_list = get_main_list(no_of_articles, search_params)
+  wiki_link = "https://commons.wikimedia.org/wiki/"
+  save_dir = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")
+  os.makedirs(save_dir, exist_ok=True)
+
+  for article in range(0, len(main_list)):
+    image_url, image_name, description , place, date, source= get_main_image(wiki_link, main_list[article], save_dir, article)
+    print("Scraping " + image_url +" ....")
+    try:
+      description = description.replace('English:', '') + " ca. " + date.replace('\n', ' ')
+    except:
+      description = ""
+
+
+    new_row =  {'url': image_url,
+      'title': main_list[article][5:],
+      'description': description,
+      'source': source,
+      'place': place,
+      'depicted place': place,
+      'image_name': image_name}
+
+    df = df._append(new_row, ignore_index=True)
+    csv_file_path = f"{save_dir}/AllWikiData.csv"
+    try:
+        df.to_csv(csv_file_path, index=False)
+        print(image_url + " data has been saved to:", csv_file_path)
+    except PermissionError:
+        print("Please close the 'AllWikiData.csv' file and Restart the scraper. You can't use the scraper and have 'AllWikiData.csv' open simultaneously")
+else:
+  wiki_link = "https://commons.wikimedia.org/wiki/"
+  save_dir = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")
+  os.makedirs(save_dir, exist_ok=True)
+
+  image_url = url_scraped
+  print("Scraping " + image_url +" ....")
+  url_parsed = urlparse(image_url)
+  article = url_parsed[2][6:]
+
+  image_url, image_name = get_main_image(wiki_link, article, save_dir, 1)
+  description , place, date, source = get_excel_data(image_url)
+  if description != None:
+
+    try:
+      description = description.replace('English:', '') + " ca. " + date.replace('\n', ' ')
+    except:
+      description = ""
+    new_row =  {'url': image_url,
+        'title': article[5:],
+        'description': description,
+        'source': source,
+        'place': place,
+        'depicted place': place,
+        'image_name': image_name}
+
+    df = df._append(new_row, ignore_index=True)
+    csv_file_path = f"{save_dir}/AllWikiData.csv"
+    try:
+        df.to_csv(csv_file_path, index=False)
+        print(image_url + " data has been saved to:", csv_file_path)
+    except PermissionError:
+        print("Please close the 'AllWikiData.csv' file and Restart the scraper. You can't use the scraper and have 'AllWikiData.csv' open simultaneously")
+  else:
+    print("Invalid Link, Please check the link and restart the scraper!")
+
---
+++ scraper 2/main.py
@@ -0,0 +1,342 @@
+from urllib.parse import urlparse, parse_qs
+import urllib
+from datetime import datetime, timedelta
+import requests
+import os
+import subprocess
+import sys
+
+def check_num_power(num):
+    return round_to_nearest_ten(int(num))
+
+def round_to_nearest_ten(number):
+    return round(number, -1)
+
+def time_a_day_from_now():
+    now = datetime.utcnow()
+    future_time = now - timedelta(days=1)
+    return str(future_time.strftime("%Y-%m-%dT%H:%M:%SZ"))
+
+def time_a_week_from_now():
+    now = datetime.utcnow()
+    future_time = now - timedelta(weeks=1)
+    return str(future_time.strftime("%Y-%m-%dT%H:%M:%SZ"))
+
+def time_a_month_from_now():
+    now = datetime.utcnow()
+    future_time = now - timedelta(days=30)
+    return str(future_time.strftime("%Y-%m-%dT%H:%M:%SZ"))
+
+def time_a_year_from_now():
+    now = datetime.utcnow()
+    future_time = now - timedelta(days=365)
+    return str(future_time.strftime("%Y-%m-%dT%H:%M:%SZ"))
+
+def get_image_location(asset):
+    # A, B and C are present
+    if asset['location']['city'] and asset['location']['state'] and asset['location']['country']:
+        image_location = asset['location']['city'] + ", " +  asset['location']['state'] + ", " +  asset['location']['country']
+    # A and C are present
+    elif asset['location']['city'] and asset['location']['country']:
+        image_location = asset['location']['city'] + ", " +  asset['location']['country']
+    # A and B are present
+    elif asset['location']['city'] and asset['location']['state']:
+        image_location = asset['location']['city'] + ", " +  asset['location']['state']
+    # B and C are present
+    elif asset['location']['state'] and asset['location']['country']:
+        image_location = asset['location']['state'] + ", " +  asset['location']['country']
+    # Only A is present
+    elif asset['location']['city']:
+        image_location = asset['location']['city']
+    # Only B is present
+    elif asset['location']['state']:
+        image_location = asset['location']['state']
+    # Only C is present
+    elif asset['location']['country']:
+        image_location = asset['location']['country']
+    else:
+        image_location = "NIL"
+    return image_location
+
+def parse_url(url):
+    parsed_url = urlparse(url)
+    query_params = parse_qs(parsed_url.query)
+    return query_params
+
+def convert_date_format(date_value):
+    if '-' in date_value:
+        return date_value.split("-")[0][:4]+ "-" + date_value.split("-")[0][4:6] + "-" + date_value.split("-")[0][6:] + "T00:00:00Z", date_value.split("-")[1][:4]+ "-" + date_value.split("-")[1][4:6] + "-" + date_value.split("-")[1][6:] + "T23:59:59Z"
+    elif date_value == '1d':
+        return time_a_day_from_now(), None  # Implement time_a_day_from_now() function
+    elif date_value == '1w':
+        return time_a_week_from_now(), None  # Implement time_a_week_from_now() function
+    elif date_value == '1m':
+        return time_a_month_from_now(), None  # Implement time_a_month_from_now() function
+    elif date_value == '1y':
+        return time_a_year_from_now(), None # Implement time_a_year_from_now() function
+    else:
+        return None, None
+
+def refactor_params(params):
+    searchQueryList = ['branch', 'date', 'cocom', 'unit', 'country', 'state', 'credit', 'sort', 'type']
+    refactored_params = {}
+    for key, value in params.items():
+
+        if "filter[" in key:
+          key = key.replace("filter[", '').replace(']', '')
+        elif "filter\\" in key:
+          key = key.replace("filter\\", '').replace('[','').replace(']', '')
+
+        if key == 'date':
+            from_date, to_date = convert_date_format(value[0])
+            if from_date and to_date:
+                refactored_params['from_date'] = from_date
+                refactored_params['to_date'] = to_date
+            elif from_date:
+              refactored_params['from_date'] = from_date
+            else:
+              refactored_params[key] = value[0]
+        elif key == 'journalist':
+          refactored_params['credit'] = value[0]
+        elif key in searchQueryList:
+            refactored_params[key] = value[0]
+    return refactored_params
+
+
+headers = {
+    "Accept-Encoding": "gzip, deflate",
+    "User-Agent": "Zend_Http_Client",
+    "Accept": "application/json, text/javascript",
+    "Referer": "https://api.dvidshub.net"
+}
+
+def search_dvidshub_api(search_params):
+    search_params['max_results'] = 50
+    search_params['fields'] = ['id']
+    search_params['api_key'] = api_key
+    url = "https://api.dvidshub.net/search"
+    response = requests.get(url, params=search_params, headers=headers)
+    try:
+      return response.json()['results']
+    except:
+      return response.json()
+
+def asset_dvidshub_api(id):
+    params = {
+    "id": id,
+    "api_key": "key-65f2f73501a82"
+    }
+    url = "https://api.dvidshub.net/asset"
+    response = requests.get(url, params=params, headers=headers )
+    try:
+      return response.json()['results']  # Assuming the response is binary data (e.g., an image)
+    except:
+      return response.json()
+
+def get_total_results(search_params):
+    search_params['max_results'] = 1
+    search_params['api_key'] = api_key
+    url = "https://api.dvidshub.net/search"
+    response = requests.get(url, params=search_params, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        page_info = data.get('page_info')
+        if page_info:
+            total_results = page_info.get('total_results')
+            return total_results
+    else:
+      print(response)
+    return None
+
+def convert_to_human_readable(time_str):
+  try:
+    # Parse the time string
+    time_obj = datetime.strptime(time_str, '%Y-%d-%mT%H:%M:%S%z')
+
+    # Format the date into a human readable string
+    return str(time_obj.strftime('%B %d, %Y'))
+  except ValueError:
+    try:
+      time_obj = datetime.strptime(time_str, '%Y-%m-%dT%H:%M:%S%z')
+      return str(time_obj.strftime('%B %d, %Y'))
+    except ValueError:
+      print("Invalid_time_string: ", time_str)
+      return "Invalid time format."
+
+
+
+# Main Logic
+# Example usage:
+search_results_list = []
+api_key = "key-65f2f73501a82"
+print("****************************************************")
+print("PLEASE NOTE: This scraper only works with Dvidshub search URLs. To use it, please perform a search on Dvidshub.net, applying any filters you desire. Once you have completed your search, copy the URL, start the scraper, and paste the URL here.")
+print("****************************************************")
+url = input("Paste your url here:> ")
+print("Started Scraping...")
+params = parse_url(url)
+searchParamDict = refactor_params(params)
+print("Getting the Total Number of data...")
+totalNoData = input("How many images are you expecting (put any amount of images you want to work with if you don't know)? ")
+final_no = int(totalNoData)
+#print(check_num_power(totalNoData))
+if int(totalNoData) > 9:
+  totalNoData = check_num_power(totalNoData)
+else:
+  totalNoData = int(totalNoData)
+
+print("Getting data...\nPlease be patient, your data extraction will start soon :)")
+if totalNoData >= 50:
+  loop_iterator = round(totalNoData/50)
+else:
+  loop_iterator = 1
+
+#print(loop_iterator)
+
+for i in range(1, loop_iterator + 2):
+
+    searchParamDict['page'] = i
+    try:
+      search_results = search_dvidshub_api(searchParamDict)
+    except Exception as e:
+       print("The following error occurred: ", e,)
+       print("Report this to Tari on Upwork @ 'https://www.upwork.com/freelancers/~01cb3c617ddbf2a8a8' ")
+       input('Press any key to exit...')
+       sys.exit()
+
+    for search_result in search_results:
+        search_results_list.append(search_result['id'])
+
+current_datetime = datetime.now()
+save_dir = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")
+os.makedirs(save_dir, exist_ok=True)
+
+
+
+#print("Creating Excel sheet 'AllDVIDSHUBData.csv'...")
+print("""
+________                              __  .__
+\_____  \ ______   ________________ _/  |_|__| ____   ____
+ /   |   \\____ \_/ __ \_  __ \__  \\   __\  |/  _ \ /    \/
+/    |    \  |_> >  ___/|  | \// __ \|  | |  (  <_> )   |  \/
+\_______  /   __/ \___  >__|  (____  /__| |__|\____/|___|  /
+        \/|__|        \/           \/                    \/
+  _________ __                                .__  .__
+ /   _____//  |________   ____ _____    _____ |  | |__| ____   ____
+ \_____  \\   __\_  __ \_/ __ \\__  \  /     \|  | |  |/    \_/ __ \/
+ /        \|  |  |  | \/\  ___/ / __ \|  Y Y  \  |_|  |   |  \  ___/
+/_______  /|__|  |__|    \___  >____  /__|_|  /____/__|___|  /\___  >
+        \/                   \/     \/      \/             \/     \/
+
+""")
+
+
+
+j = 0
+for i in range(0, len(search_results_list)):
+    #print(id)
+    #asset = asset_dvidshub_api(search_results_list[i])
+    #print(asset)
+
+    try:
+      asset = asset_dvidshub_api(search_results_list[i])
+    except Exception as e:
+       print("The following error occurred: ", e,)
+       print("Report this to Tari on Upwork @ 'https://www.upwork.com/freelancers/~01cb3c617ddbf2a8a8' ")
+       input('Press any key to exit...')
+       sys.exit()
+
+    if asset.get('errors') != None:
+      continue
+
+    img_download_url = asset['image']
+    image_name = f"{j + 1}"
+    j = j + 1
+    if asset['image'][-4] == '.':
+        image_name = image_name + asset['image'][-4:]
+    else:
+        image_name = image_name + asset['image'][-5:]
+
+    try: #Image not found
+      urllib.request.urlretrieve(img_download_url, save_dir+"//"+image_name)
+    except Exception as e:
+      continue
+
+
+    image_location = get_image_location(asset).strip()
+
+    if "Undisclosed Location" in image_location:
+      image_location = "NIL"
+
+    image_title = asset['title'].strip()
+
+    if not image_title:
+      image_title = "NIL"
+
+    image_description = asset['description']
+
+    if not image_description:
+      image_description = "NIL"
+
+
+
+    image_date = convert_to_human_readable(asset['date'])
+
+    if image_description != "NIL":
+      if image_date != "Invalid time format.":
+        image_description = asset['description'].strip() + " ca. " + image_date
+      else:
+        image_description = asset['description'].strip()
+
+
+      # OPERATION STREAMLINE LOGIC
+      
+      image_file_path = save_dir + "\\" + image_name
+
+      current_directory = os.getcwd()
+
+      exiftool_file_name = 'exiftool.exe'
+
+      exiftool_file_path = os.path.join(current_directory, exiftool_file_name)
+
+      exiftool_command = [
+          exiftool_file_path,
+          '-m',
+          f'-Headline={image_description}',
+          f'-Description={image_description}',
+          f'-CreatorWebsite={asset["url"]}',
+          f'{image_file_path}']
+
+      process = subprocess.Popen(args=exiftool_command)
+
+      process.wait()
+
+      un_path = f'{image_file_path}_original'
+
+      if os.path.exists(un_path):
+          os.unlink(un_path)
+      else:
+        pass
+        continue
+        exiftool_command = [
+          exiftool_file_path,
+          '-m',
+          f'-Headline={image_description}',
+          f'-Description={image_description}',
+          f'{image_file_path}']
+
+        process = subprocess.Popen(args=exiftool_command)
+
+        process.wait()
+
+        un_path = f'{image_file_path}_original'
+
+        if os.path.exists(un_path):
+            os.unlink(un_path)
+        else:
+          pass
+
+      print(image_name + " medadata and image " + " data has been saved to:", image_file_path)
+
+print(f"All {final_no} plus surplus data has been scraped along with the metadata edited")
+input("Press Anything to close the scraper...")
